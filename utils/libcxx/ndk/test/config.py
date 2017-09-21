0c369255b9626c4b3acbf3ca706beed4d74e68f4
Dan Albert <danalbert@google.com>
2017-09-21 16:40:35 -0700

    Fix arm32 detection.
    The reason all the tests broke when I tried to fix the arm7 triple in
    the NDK test runner is because we were no longer properly detecting
    arm32. We'd use the libgcc unwinder instead and crash whenever we
    tried to unwind.
    
    Test: ndk/run_tests.py
    Bug: None
    Change-Id: I345477203f6993dc292756df0cbb8fb5b4e4d439
    (cherry picked from commit c02af0dbf54d90b323c43ab477ce06ae1412dafa)

--- utils/libcxx/ndk/test/config.py
+++ utils/libcxx/ndk/test/config.py
@@ -93,7 +93,7 @@ class Configuration(libcxx.test.config.Configuration):
 
         self.cxx.link_flags.append('-landroid_support')
         triple = self.get_lit_conf('target_triple')
-        if triple.startswith('arm-'):
+        if triple.startswith('arm-') or triple.startswith('armv7-'):
             self.cxx.link_flags.append('-lunwind')
             self.cxx.link_flags.append('-Wl,--exclude-libs,libunwind.a')
 
